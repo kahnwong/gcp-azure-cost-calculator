9f88df0c48e864557a3e37952017a1f77eabea26
Karn Wong <karn@karnwong.me>
2024-02-20 22:00:48 +0700

    add cloud run

---
+++ gcp_azure_cost_calculator/model/gcp.py
@@ -0,0 +1,56 @@
+from decimal import Decimal
+
+from pydantic import BaseModel
+from pydantic import computed_field
+
+# """
+# Ref: https://cloud.google.com/run/pricing
+# Unit: USD
+# Region: asia-southeast1 # tier 2
+# """
+
+
+class CloudRun(BaseModel):
+    # cost
+    vcpu_second: Decimal = Decimal(0.00003360)
+    memory_second: Decimal = Decimal(0.00000350)
+    request_million: Decimal = Decimal(0.40)
+
+    # user's input
+    vcpu_request: Decimal = Decimal(1.0)
+    memory_request: Decimal = Decimal(1.0)
+    execution_time_per_request_ms: Decimal = Decimal(50)
+    requests_per_month: Decimal = Decimal(10000)
+
+    @computed_field
+    @property
+    def execution_time_second(self) -> Decimal:
+        return (
+            self.execution_time_per_request_ms * self.requests_per_month / Decimal(1000)
+        )
+
+    @computed_field
+    @property
+    def cost_cpu(self) -> Decimal:
+        return self.vcpu_request * self.vcpu_second * self.execution_time_second
+
+    @computed_field
+    @property
+    def cost_memory(self) -> Decimal:
+        return self.memory_request * self.memory_second * self.execution_time_second
+
+    @computed_field
+    @property
+    def cost(self) -> Decimal:
+        return self.cost_cpu + self.cost_memory
+
+
+if __name__ == "__main__":
+    # test
+    cloud_run = CloudRun(
+        vcpu_request=2,
+        memory_request=2,
+        execution_time_per_request_ms=500,
+        requests_per_month=100000000,
+    )
+    print(f"Cloud Run: {cloud_run.cost}")
